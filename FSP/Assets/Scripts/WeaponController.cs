4c0f2dfe8470be0a7dd0998e02701db7ff6b43bf
Laura Mambrilla Moreno <lauramambrilla@gmail.com>
2024-02-04 02:29:56 +0100

    Control ammo.

--- FSP/Assets/Scripts/WeaponController.cs
+++ FSP/Assets/Scripts/WeaponController.cs
@@ -11,13 +11,19 @@ public class WeaponController : MonoBehaviour
     [SerializeField] private LayerMask hittableLayers;
 
     [Header("Transform")]
-    [SerializeField] private Transform weaponMuzzle;    // --> weaponNozzle
+    [SerializeField] private Transform weaponNozzle;    
     private Transform playerCameraTransform;
 
+    [Header("Positions")]
+    [SerializeField] private Vector3 initialPosition;
+
     [Header("Shoot parameters")]
-    private float fireDistance;
-    private float recoilForce;
-    private Vector3 initialPosition;
+    [SerializeField] private float fireDistance = 200;
+    [SerializeField] private float fireRate = 0.6f;    // intervalo entre cada disparo
+    [SerializeField] private float recoilForce = 4f;
+    [SerializeField] private int maxNumAmmo = 8;
+    [SerializeField] public int currentNumAmmo { get; private set; }
+    [SerializeField] private float lastTimeShoot = Mathf.NegativeInfinity;
 
     [Header("Sound and visual effects")]
     [SerializeField] private GameObject flashEffect;
@@ -29,38 +35,36 @@ public class WeaponController : MonoBehaviour
         initialPosition = transform.localPosition;
         playerCameraTransform = GameObject.FindGameObjectWithTag("PlayerCamera").transform;
 
-        fireDistance = 200;
-        recoilForce = 4f;
+        currentNumAmmo = 8;
     }
 
     // Update is called once per frame
     void Update()
     {
-        HandleShoot();
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (CanShoot())
+            {
+                HandleShoot();
+            }
+        }
 
         transform.localPosition = Vector3.Lerp(transform.localPosition, initialPosition, Time.deltaTime * 5f);
     }
 
     private void HandleShoot()
     {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Flash();
-            Recoil();
+        Flash();
+        Recoil();
+        BulletHole();
 
-            RaycastHit hit;
-
-            if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out hit, fireDistance, hittableLayers))
-            {
-                GameObject bulletHoleClone = Instantiate(bulletHole, hit.point + hit.normal * 0.001f, Quaternion.LookRotation(hit.normal));
-                Destroy(bulletHoleClone, 5f);
-            }
-        }
+        currentNumAmmo--;
+        lastTimeShoot = Time.time;
     }
 
     private void Flash()
     {
-        GameObject flashClone = Instantiate(flashEffect, weaponMuzzle.position, Quaternion.Euler(weaponMuzzle.forward), transform);
+        GameObject flashClone = Instantiate(flashEffect, weaponNozzle.position, Quaternion.Euler(weaponNozzle.forward), transform);
         Destroy(flashClone, 1f);
     }
 
@@ -70,5 +74,29 @@ public class WeaponController : MonoBehaviour
         transform.position = transform.position - transform.forward * (recoilForce / 50f);
     }
 
-    
+    private void BulletHole()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out hit, fireDistance, hittableLayers))
+        {
+            GameObject bulletHoleClone = Instantiate(bulletHole, hit.point + hit.normal * 0.001f, Quaternion.LookRotation(hit.normal));
+            Destroy(bulletHoleClone, 5f);
+        }
+    }
+
+    private bool CanShoot()
+    {
+        bool shoot = false;
+
+        if (currentNumAmmo > 0)
+        {
+            if ((lastTimeShoot + fireRate) < Time.time)
+            {
+                shoot = true;
+            }
+        }
+
+        return shoot;
+    }
 }
